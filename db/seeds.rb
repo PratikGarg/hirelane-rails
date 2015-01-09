77006a18e9cc4b3fef81f84d3dff5223d380aedc
priyank <priyank1985gupta@rediffmail.com>
2015-01-09 16:53:01 +0530

    added users

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,9 +10,28 @@
 @role2 = Role.new name: 'ADMIN'
 @role2.save!
 
-@user = User.new email: 'admin@example.com'
-@user.password = 'pass123654'
-@user.password_confirmation = 'pass123654'
-@user.save!
+@user1 = User.new email: 'pgarg@xebia.com'
+@user1.password = 'password'
+@user1.password_confirmation = 'password'
+@user1.save!
 
+@user2 = User.new email: 'pgupta@xebia.com'
+@user2.password = 'password'
+@user2.password_confirmation = 'password'
+@user2.save!
+
+@user3 = User.new email: 'bsingh@xebia.com'
+@user3.password = 'password'
+@user3.password_confirmation = 'password'
+@user3.save!
+
+@user4 = User.new email: 'vsehgal@xebia.com'
+@user4.password = 'password'
+@user4.password_confirmation = 'password'
+@user4.save!
+
+@user5 = User.new email: 'deepubansal@xebia.com'
+@user5.password = 'password'
+@user5.password_confirmation = 'password'
+@user5.save!
 
