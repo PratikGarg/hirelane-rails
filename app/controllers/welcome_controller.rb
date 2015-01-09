5c6e02eca95c26564e2eec2d0d22f9f80b259289
Vaibhav Sehgal <vsehgal@xebia.com>
2015-01-09 21:40:38 +0530

    Make dashboard dynamic. It should pick candidates from the database

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,11 +1,7 @@
 class WelcomeController < ApplicationController
 	before_action :authenticate_user!
+	
 	def index
-		# User.all.each do |user|
-		# 	Thread.new do
-		# 		UserMailer.welcome_message(user).deliver
-		# 	end
-		# end
-
-	end
+    	@candidates = Candidate.all
+  	end
 end
